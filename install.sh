0852dc82b990920dfff840d1781687f4b3904bde
Paolo Brito <51763055+PaoloBrito01@users.noreply.github.com>
2024-11-06 11:37:24 -0400

    Change in MD file
    add archive .sh to install packages

---
+++ install.sh
@@ -0,0 +1,10 @@
+# Pacotes para instalação das bibliotecas nescessárias
+# Criar um ambiente conda.
+conda create --name lfa
+# Ativando ambiente
+conda activate lfa
+#Instalar os seguintes pacotes:
+pip install pyqt5
+pip install graphviz
+
+
